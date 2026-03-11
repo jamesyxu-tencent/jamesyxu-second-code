4f8366d3219bf97a1659cd52f56f3357bcfe7d78
xuyangm <xuysmail@163.com>
2026-03-11 22:37:51 +0800

    commit

--- src/main/java/org/example/controller/AiHelloWorldController.java
+++ src/main/java/org/example/controller/AiHelloWorldController.java
@@ -13,7 +13,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
